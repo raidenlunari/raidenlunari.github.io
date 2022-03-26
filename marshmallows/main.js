d257560d94c87a6f321c32e1f44759fd95426864
raidenlunari <lunari.is.gaming@gmail.com>
2022-03-25 20:48:28 -0700

    improve marshmallows

--- marshmallows/main.js
+++ marshmallows/main.js
@@ -1,9 +1,22 @@
 var gameData = {
     marsh: 0,
-    marshPerClick: 1
+    marshPerClick: 1,
+    marshPerClickCost: 10
   }
   
   function duplicateMarsh() {
     gameData.marsh += gameData.marshPerClick
     document.getElementById("marshDuplicated").innerHTML = gameData.marsh + " Marshmallows Duplicated"
   }
+
+  function dupMarshPerClick() {
+    if (gameData.marsh >= gameData.marshPerClickCost) {
+      gameData.marsh -= gameData.marshPerClickCost
+      gameData.marshPerClick += 1
+      gameData.marshPerClick *= 2
+    }
+  }
+
+  var mainGameLoop = window.setInterval(function() {
+    duplicateMarsh()
+  }, 1000)
